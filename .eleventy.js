aaab81af91532b8fb3da3e9c107a6b2a6a594e73
dylnbk <dyln.bk@gmail.com>
2022-05-29 03:11:16 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("media");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addWatchTarget("css");
+
+    const {
+        DateTime
+      } = require("luxon");
+    
+        eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+          return DateTime.fromJSDate(dateObj, {
+            zone: 'utc'
+          }).toFormat('yy-MM-dd');
+        });
+    
+        eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {
+          zone: 'utc'
+        }).toFormat("dd-MM-yy");
+      });
+    
+};
---
+++ posts/test-post copy 10.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/post.njk
+title: Test post 11
+description: Just a test
+date: 2022-05-25
+featuredImage: /media/uploads/test.jpg 
+---
+
+Text for the post is here
